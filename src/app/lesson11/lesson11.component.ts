b1cc47451386c88ca6d9e1550fcd71a359eec88f
alexandre <xanddybr@gmail.com>
2024-12-18 01:39:48 +0000

    ngStyle

--- src/app/lesson11/lesson11.component.ts
+++ src/app/lesson11/lesson11.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lesson11',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lesson11.component.html',
   styleUrl: './lesson11.component.scss'
 })
 export class Lesson11Component {
+
+  optionColor:string = 'blue'
+  optionText:string[] = ['very','very','good']
 
 }
