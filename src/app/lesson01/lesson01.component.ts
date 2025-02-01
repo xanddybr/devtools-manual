c7cb3b60879594ab3e0fea2e176eec10a7496904
xanddybr <xanddybr@gmail.com>
2025-02-01 05:14:05 -0300

    teste fetch cep method

--- src/app/lesson01/lesson01.component.ts
+++ src/app/lesson01/lesson01.component.ts
@@ -1,13 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Cep } from './../models/Cep';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { data } from 'jquery';
 
 @Component({
   selector: 'app-lesson01',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lesson01.component.html',
   styleUrl: './lesson01.component.scss'
 })
-export class Lesson01Component {
+export class Lesson01Component implements OnInit {
+
+  ngOnInit(): void {
+   
+    const button1 = document.getElementById('button')
+
+    button1.addEventListener('click', ()=> {
+      this.loadData()
+    })
+  }
+
+  dataCep:Cep[]=[]
+
+  loadData(){
+    const url = "https://viacep.com.br/ws/RS/Porto%20Alegre/Domingos/json/"
+    fetch(url)
+    .then(response => {
+      if(!response.ok) {
+        alert('erro ao carregar a lista de ceps')
+      }
+      response.json()
+      .then(data => { if(this.dataCep.length == 0 ){
+        this.dataCep = data as Cep[]
+      } })
+      .finally(()=>{console.log('Concluido com sucesso!')})
+    })
+    
+  }
+
 
 
 
---
+++ src/app/models/Cep.ts
@@ -0,0 +1,9 @@
+export class Cep {
+
+    cep:string
+    logradouro:string
+    bairro:string
+    estado:string
+    ibge:string
+   
+}
